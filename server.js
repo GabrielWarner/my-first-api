e8e124660c2534e7e2f188cfe91975a8e66176a0
GabrielWarner <gabe0412@uw.edu>
2022-07-24 16:42:56 -0700

    created API get request to see the data using /api/family

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 //importing express for the server and path so that it can be used with public folder
 const express = require('express');
 const path = require('path');
+//importing helper function to read from file
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 
 //initialize app
 const app = express();
@@ -18,6 +20,13 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+//get request for my family API
+app.get ('/api/family', (req, res) => {
+    console.log("get request received for family")
+    //using helper function to read the data in the database folder and then parse it to json
+    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
+})
 
 //set the app to listen to PORT constant
 app.listen(PORT, () =>
