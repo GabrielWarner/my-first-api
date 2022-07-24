2108b67e71761e95282c2f3876df67dfe3978ddd
GabrielWarner <gabe0412@uw.edu>
2022-07-24 16:31:02 -0700

    set up server and get request to send back html file

--- server.js
+++ server.js
@@ -10,3 +10,16 @@ const PORT = 3001;
 //telling express to use the static folder called public
 app.use(express.static('public'));
 
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//get request for sending back the index.html
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+//set the app to listen to PORT constant
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
